948c7e91614cf49e6e6f96809f11775818019763
BuildTools <unconfigured@null.spigotmc.org>
2022-06-29 22:17:26 +0200

    All events

--- src/main/java/pl/poleq/discordoos/events/Events.java
+++ src/main/java/pl/poleq/discordoos/events/Events.java
@@ -12,6 +12,7 @@ public class Events
         bot.addEventListener(new OnJoin());
         bot.addEventListener(new OnPing());
         bot.addEventListener(new AllEvents());
+        bot.addEventListener(new Funfacts());
         new OnEnable(bot);
     }
 }
